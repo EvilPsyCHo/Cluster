b9c951fabad700ba5185017fd8d950690441e1c7
evilpsycho <evilpsycho@icloud.com>
2018-10-18 16:01:30 +0800

    kmeans

--- build/lib/cluster/evaluate.py
+++
@@ -1,5 +0,0 @@
-# coding:utf8
-# @Time    : 18-10-17 上午10:57
-# @Author  : evilpsycho
-# @Mail    : evilpsycho42@gmail.com
-from sklearn.metrics import adjusted_rand_score
--- build/lib/cluster/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding:utf8
-# @Time    : 18-10-16 下午6:31
-# @Author  : evilpsycho
-# @Mail    : evilpsycho42@gmail.com
-from .external import *
